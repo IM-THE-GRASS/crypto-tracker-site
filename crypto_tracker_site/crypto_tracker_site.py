69988ee82948372b7b6db8ac2df1ddbe4cacb6df
IM-THE-GRASS <81999763+IM-THE-GRASS@users.noreply.github.com>
2024-08-31 21:05:53 -0600

    working

--- crypto_tracker_site/crypto_tracker_site.py
+++ crypto_tracker_site/crypto_tracker_site.py
@@ -1,32 +1,59 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
-
-from rxconfig import config
+import requests
 
 
 class State(rx.State):
-    """The app state."""
+    name = "Bitcoin"
+    rank = "#1"
+    symbol = "BTC"
+    usd_value = 59083.73
+    current_input:str
+    data:list
+    coin_index:int = 1
+    @rx.var()
+    def result(self):
+        try:
+            return str(round(float(self.current_input) * self.usd_value)) + " USD"
+        except:
+            return "0 USD"
+    def get_coin_info(self):
+        coin = self.data[self.coin_index]
+        self.name = coin["name"]
+        self.symbol = coin["symbol"]
+        self.usd_value = float(coin["price_usd"])
+        self.rank = "#" + str(coin["rank"])
+    def on_load(self):
+        response = requests.get("https://api.coinlore.net/api/tickers/")
+        data = response.json()["data"]
+        self.data = data
+        self.get_coin_info()
+        
+    def next(self):
+        self.coin_index = (self.coin_index + 1) % len(self.data)
+        self.get_coin_info()
+    def back(self):
+        self.coin_index = (self.coin_index - 1) % len(self.data)
+        self.get_coin_info()
+    def on_change(self, new):
+        self.current_input = new
 
-    ...
-
-
+@rx.page(on_load=State.on_load)
 def index() -> rx.Component:
-    # Welcome Page (Index)
     return rx.container(
         rx.center(
             rx.hstack(
                 rx.text(
-                    "Bitcoin",
+                    State.name,
                     font_weight="900",
                     font_size="256px",
                     letter_spacing="-7.6px",
                     height="225px",
-                    line_height="256px"
+                    line_height="256px",
+                    text_wrap="nowrap"
                 ),
                 rx.center(
                     rx.text(
-                        "#1",
+                        State.rank,
                         font_size="96px",
                         color="#C7C7C7",
                         line_height="96px"
@@ -46,7 +73,7 @@ def index() -> rx.Component:
                 rx.input(
                     rx.center(
                         rx.text(
-                            "BTC",
+                            State.symbol,
                             font_weight="bold",
                             font_size="64px"
                         ),
@@ -57,13 +84,29 @@ def index() -> rx.Component:
                     width="755px",
                     height="98px",
                     border = "1px solid #626262",
-                    font_size="48px"
+                    font_size="48px",
+                    
+                    value=State.current_input,
+                    on_change=State.on_change
                     
                 ),
-                rx.text(
-                    "=",
-                    font_size="256px",
-                    line_height="24px"
+                rx.center(
+                    rx.image(
+                        src="/=.png"
+                    ),
+                    width="755px"
+                ),
+                rx.center(
+                    rx.text(
+                        State.result,
+                        height="113px",
+                        font_size="128px",
+                        line_height="80px",
+                        margin_right="60px",
+                        text_wrap="nowrap",
+                        font_weight="bolder"
+                    ),
+                    width="755px"
                 ),
                 spacing="0",
                 gap = "55px"  
@@ -73,6 +116,20 @@ def index() -> rx.Component:
             height="429px",
             left="0px",
             top="309px"
+        ),
+        rx.image(
+            src="/arrow_back.png",
+            position="absolute",
+            left="0px",
+            top="287px",
+            on_click=State.back
+        ),
+        rx.image(
+            src="/arrow_forward.png",
+            position="absolute",
+            left="1909px",
+            top="287px",
+            on_click= State.next
         )
     )
 
